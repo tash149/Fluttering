ef37c395abef1e70370b2ea40e3382cbc40c0236
Tarushi Sharma <tarushisharmaace@gmail.com>
2018-11-15 03:04:09 +0530

    page1 not working

--- log_sign/lib/splash_sc/Page1.dart
+++ log_sign/lib/splash_sc/Page1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 
 class Page1 extends StatefulWidget {
   @override
@@ -7,22 +8,52 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
+  String myText;
+  StreamSubscription<DocumentSnapshot> subscription;
 
-  Future _getPlaceName() async {
-    /*documentReference = Firestore.instance.document('user/skills');
-    DocumentSnapshot snapshot = await documentReference.get();
-    return snapshot;*/
+  final DocumentReference documentReference = Firestore.instance.document("pTq8Tl2KuWgYEpblue6453LgCpm1/place_Name");
 
-    var firestore = Firestore.instance;
-    await firestore.settings(timestampsInSnapshotsEnabled: true);
-    QuerySnapshot qn = await firestore.collection("user/pTq8Tl2KuWgYEpblue6453LgCpm1").getDocuments();
-    return qn.documents;
+  void _fetch() {
+    documentReference.get().then((datasnapshot) {
+      if (datasnapshot.exists) {
+        setState(() {
+          myText = datasnapshot.data['place_Name'];
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    subscription = documentReference.snapshots().listen((datasnapshot) {
+      if (datasnapshot.exists) {
+        setState(() {
+          myText = datasnapshot.data['place_Name'];
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        backgroundColor: Colors.green,);
+        backgroundColor: Colors.green,
+        body:new Container(
+          //margin: EdgeInsets.only(left: 10.0, right: 10.0),
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
+            child:new Card(
+                child: new Container(
+                    padding:EdgeInsets.only(right: 15.0),
+                    child: Text(myText,
+                        style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold)
+                    ),
+                    //alignment: Alignment(0.0, 0.0),
+                  ),
+            )
+        )
+    );
   }
 }
 
